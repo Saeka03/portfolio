1e0f82f088b36b3f7dd6c8407b4e4391ec36d6e7
Saeka Kubota <hdr23.nknkn@gmail.com>
2024-09-30 23:18:34 -0700

    Implement a home content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./page.module.scss";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 export default function Page() {
   return (
     <div>
       <Navbar />
+      <Home />
       <h1 className={styles.next}>Hello, Next.js!</h1>
     </div>
   );
