579b4ce9b4b40827259e21b499677a444f95faee
Saeka03 <143541044+Saeka03@users.noreply.github.com>
2024-09-30 23:19:33 -0700

    Implement a home content (#3)

---
+++ src/app/components/Home.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import React from "react";
+import styles from "./Home.module.scss";
+
+function Home() {
+  return (
+    <div className={styles.home}>
+      <div className={styles.me}>
+        <Image
+          className={styles.myImg}
+          src={"/me.svg"}
+          alt={"My illust"}
+          width={200}
+          height={225}
+        />
+      </div>
+      <div className={styles.line}>
+        <Image
+          className={styles.speechBubbleImg}
+          src={"/speech_bubble1.svg"}
+          alt={"Speech bubble 1"}
+          width={200}
+          height={225}
+        />
+        <div className={styles.text}>Hi, I am Saeka!</div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
