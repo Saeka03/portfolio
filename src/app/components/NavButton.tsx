efab6465b276a3d6a03396167c5942b4d0fae418
Saeka Kubota <hdr23.nknkn@gmail.com>
2024-10-04 05:16:31 -0700

    Make the Navbar movement

--- src/app/components/NavButton.tsx
+++ src/app/components/NavButton.tsx
@@ -3,20 +3,18 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import styles from "./NavButton.module.scss";
+import { IconType } from "react-icons";
 
 type NavButtonProps = {
-  img: string;
-  alt: string;
-  width: number;
-  height: number;
-  text: string;
+  name: string;
+  icon: IconType;
 };
 
-function NavButton({ img, alt, width, height, text }: NavButtonProps) {
+function NavButton({ name, icon: Icon }: NavButtonProps) {
   const [hovered, setHovered] = useState<string | null>(null);
 
-  const handleMouseEnter = (text: string) => {
-    setHovered(text);
+  const handleMouseEnter = (name: string) => {
+    setHovered(name);
   };
 
   const handleMouseLeave = () => {
@@ -24,17 +22,28 @@ function NavButton({ img, alt, width, height, text }: NavButtonProps) {
   };
 
   return (
-    <button
-      className={styles.navButton}
-      onMouseEnter={() => handleMouseEnter(text)}
-      onMouseLeave={handleMouseLeave}
-    >
-      <Image src={img} alt={alt} width={width} height={height} />
-      {hovered === text ? <div className={styles.text}>{text}</div> : ""}
-    </button>
+    <div className={styles.buttonContainer}>
+      <button
+        className={styles.navButton}
+        onMouseEnter={() => handleMouseEnter(name)}
+        onMouseLeave={handleMouseLeave}
+      >
+        <Icon size={40} />    
+        <div
+          className={`${styles.text} ${
+            hovered === name ? styles.showText : ""
+          }`}
+        >
+          {name}
+        </div>
+      </button>
+      <div
+        className={`${styles.underLine} ${
+          hovered === name ? styles.showUnderLine : ""
+        }`}
+      ></div>
+    </div>
   );
 }
-
-type Props = {};
 
 export default NavButton;
