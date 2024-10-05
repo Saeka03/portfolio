19da4df654ed8e9b9b1c41a3dcdb5da3b6f17196
Saeka Kubota <hdr23.nknkn@gmail.com>
2024-10-04 22:38:04 -0700

    Make the TS and JS icons rotate

--- src/app/components/OtherSkills.tsx
+++ src/app/components/OtherSkills.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./OtherSkills.module.scss";
+import { motion } from "framer-motion";
 
 function OtherSkills() {
+  const [hovered, setHovered] = useState<string | null>(null);
   return (
     <div className={styles.container}>
       <Image
@@ -12,15 +16,31 @@ function OtherSkills() {
         width={100}
         height={100}
       />
-      <Image
-        className={styles.icon}
+      <motion.img
+        className={styles.js}
+        onMouseEnter={() => setHovered("js")}
+        onMouseLeave={() => setHovered(null)}
+        animate={hovered === "js" ? { rotate: 360 } : { rotate: 0 }}
+        transition={{
+          repeat: hovered === "js" ? Infinity : 0,
+          duration: 1,
+          ease: "linear",
+        }}
         src={"js.svg"}
         alt="JS"
         width={50}
         height={50}
       />
-      <Image
-        className={styles.icon}
+      <motion.img
+        className={styles.ts}
+        onMouseEnter={() => setHovered("ts")}
+        onMouseLeave={() => setHovered(null)}
+        animate={hovered === "ts" ? { rotate: 360 } : { rotate: 0 }}
+        transition={{
+          repeat: hovered === "ts"  ? Infinity : 0,
+          duration: 1,
+          ease: "linear",
+        }}
         src={"ts.svg"}
         alt="TS"
         width={50}
