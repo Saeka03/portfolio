fe2fe9d24154699d57addca50282b795c8ad4939
Saeka Kubota <hdr23.nknkn@gmail.com>
2024-10-05 04:15:34 -0700

    Make the backend icons bounce

--- src/app/components/BackendSkills.tsx
+++ src/app/components/BackendSkills.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 import styles from "./BackendSkills.module.scss";
+import { motion } from "framer-motion";
 
 function BackendSkills() {
   return (
@@ -12,22 +15,40 @@ function BackendSkills() {
         width={100}
         height={100}
       />
-      <Image
-        className={styles.icon}
+      <motion.img
+        className={styles.nodejs}
+        whileInView={{ translateY: 200, translateX: 10, rotate: 60 }}
+        transition={{
+          type: "spring",
+          stiffness: 100,
+          damping: 10,
+        }}
         src={"nodejs.svg"}
         alt="Node.js"
         width={50}
         height={50}
       />
-      <Image
-        className={styles.icon}
+      <motion.img
+        className={styles.postgresql}
+        whileInView={{ translateY: 200, rotate: -30 }}
+        transition={{
+          type: "spring",
+          stiffness: 100,
+          damping: 10,
+        }}
         src={"postgresql.svg"}
         alt="PostgreSQL"
         width={50}
         height={50}
       />
-      <Image
-        className={styles.icon}
+      <motion.img
+        className={styles.prisma}
+        whileInView={{ translateY: 200, translateX: 30, rotate: 90 }}
+        transition={{
+          type: "spring",
+          stiffness: 100,
+          damping: 10,
+        }}
         src={"prisma.svg"}
         alt="Prisma"
         width={50}
