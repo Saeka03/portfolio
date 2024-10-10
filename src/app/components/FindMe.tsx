459801c1255613669b47e1976faec7158edea43d
Saeka Kubota <hdr23.nknkn@gmail.com>
2024-10-10 15:14:37 -0700

    adjust the size on the aboutMe component

--- src/app/components/FindMe.tsx
+++ src/app/components/FindMe.tsx
@@ -7,13 +7,14 @@ function FindMe() {
     <div className={styles.line}>
       <Image
         className={styles.speechBubbleImg}
-        src={"/speech_bubble2.svg"}
+        src={"/speech_bubble2.png"}
         alt="Speech bubble 2"
         width={225}
         height={225}
       />
       <div className={styles.text}>
-        I am a frontend web developer Find me on!
+        I am a frontend web developer.
+        <br /> Find me on!
       </div>
     </div>
   );
